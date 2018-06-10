10e1426a8c2d1106603b9dfc4c4455dfca8f9a9d
David Moeller <Davidmoeller115963@Gmail.com>
2018-06-10 10:16:13 -0400

    imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AppComponent } from './app.component';
 import { environment } from '../environments/environment';
 import { TodoComponent } from './todo/todo.component';
+import { TodoService } from './todo/Shared/todo.service';
 
 @NgModule({
   declarations: [
@@ -14,9 +15,11 @@ import { TodoComponent } from './todo/todo.component';
     TodoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule
   ],
-  providers: [],
+  providers: [TodoService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
