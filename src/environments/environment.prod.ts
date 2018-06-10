62dc77e9c15c23ed2b6ff7330503598578cabf4b
David Moeller <Davidmoeller115963@Gmail.com>
2018-06-10 10:49:03 -0400

    remove

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDvUl-_9AN6AHf5GYEPB74jsRI5Hl1Ync8",
+    authDomain: "to-do-list-db2fc.firebaseapp.com",
+    databaseURL: "https://to-do-list-db2fc.firebaseio.com",
+    projectId: "to-do-list-db2fc",
+    storageBucket: "to-do-list-db2fc.appspot.com",
+    messagingSenderId: "360398396936"
+  }
+};
